3aed2d22249b26909d6aab5ea5fdf287020749a4
Anton <levantonio@mail.ru>
2022-12-04 04:01:32 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 47. 
+// Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+    double[,] matrix = new double[rows, columns]; //строка и колонка
+
+    Random rnd = new Random(); //надо делать так
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            double num = rnd.NextDouble() * (max - min) + min;
+            matrix[i,j] = Math.Round(num, 1); //i = индекс массива
+        }
+    }
+    return matrix;
+}
+ 
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1)
+                Console.Write($"{matrix[i, j], 4 }, "); //выводим последний элемент без запятой в конце!!!!!
+            else
+                Console.Write($"{matrix[i, j], 4} ");
+        }
+    Console.WriteLine("]");
+    }
+    
+}
+
+
+
+double[,] arr2D = CreateMatrixRndDouble(3, 4, -9, 9);
+PrintMatrix(arr2D);
+
+
+   
+
+
+
+
